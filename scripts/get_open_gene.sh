ad8466976e45dd393d06a47234039dd2ce5c9a39
gmoneyomics <agershm2@jhmi.edu>
2019-06-03 12:54:48 -0400

    remove other scripts for finding coords

--- scripts/get_open_gene.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# make bed file of region of interest
-ref=/kyber/Data/Nanopore/Analysis/gmoney/CHM13/chrX_fixedBionanoSV_centromereV3.fasta
-gr38=/home/gmoney/c9orf72/ref/chrx.fasta
-
-printf 'chrX_fixedBionanoSV_centromereV3\t59060000\t59250000%s\n' > region_tmp.bed
-bedtools getfasta -fi $ref -bed region_tmp.bed -fo open_region_tmp.fasta
-minimap2 $gr38 open_region_tmp.fasta > minimap_region_ref.tsv
