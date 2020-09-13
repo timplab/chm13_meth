64109f7a3106f24a93d9e590c400d0eac39dd955
gmoneyomics <agershm2@jhmi.edu>
2020-09-13 15:14:21 -0400

    add hg002 scripts

--- scripts/hg002/call_meth.sh
+++ scripts/hg002/call_meth.sh
@@ -1,20 +1,20 @@
 #!/bin/bash 
 fastq=/uru/Data/Nanopore/Analysis/gmoney/chm13/hg002/cenX_reads_hg002_lt100kb.fastq
 fast5=/uru/Data/Nanopore/Analysis/gmoney/chm13/hg002/filteredFiles
-bam=/uru/Data/Nanopore/Analysis/gmoney/chm13/hg002/cenX_reads_hg002_lt100kb.asm8_primary.tig00021361.sorted.bam
-out=/uru/Data/Nanopore/Analysis/gmoney/chm13/hg002/nanopolish
+bam=/uru/Data/Nanopore/Analysis/gmoney/chm13/hg002/fixed_alignments/asm8-primary-tig00021361_fixed.bam
+out=/uru/Data/Nanopore/Analysis/gmoney/chm13/hg002/nanpolish_v13
 repo=/home/isac/Software/nanopolish
-ref=/uru/Data/Nanopore/Analysis/gmoney/chm13/hg002/asm8_primary.tig00021361.fa
-base=asm8_primary.tig00021361
+ref=/uru/Data/Nanopore/Analysis/gmoney/chm13/hg002/old_alignments/asm8_primary.tig00021361.fa
+base=asm8_primary.tig00021361_fixed
 
 if [ "$1" == "nanopolish" ]; then
 
-        ${repo}/nanopolish index -d ${fast5} ${fastq}
-	${repo}/nanopolish call-methylation -t 72 --reads ${fastq} --bam ${bam} --genome ${ref} > ${out}/${base}_methylation_calls.tsv
-        ${repo}/scripts/calculate_methylation_frequency.py ${out}/${base}_methylation_calls.tsv > ${out}/${base}_methylation_frequency.tsv
+       # ${repo}/nanopolish index -d ${fast5} ${fastq}
+#	${repo}/nanopolish call-methylation -t 72 --reads ${fastq} --bam ${bam} --genome ${ref} > ${out}/${base}_methylation_calls.tsv
+    #    ${repo}/scripts/calculate_methylation_frequency.py ${out}/${base}_methylation_calls.tsv > ${out}/${base}_methylation_frequency.tsv
 
 
-        python3 /home/gmoney/repos/nanopore-methylation-utilities/mtsv2bedGraph_upperlower.py -c 1.5  -i ${out}/${base}_methylation_calls.tsv -g $ref > ${out}/${base}_methylation.tmp
+        python3 /home/gmoney/repos/nanopore-methylation-utilities/mtsv2bedGraph_upperlower.py -c 1.5  -i ${out}/methylation_tq.tsv -g $ref > ${out}/${base}_methylation.tmp
         cat ${out}/${base}_methylation.tmp | sort -k1,1 -k2,2n | bgzip > ${out}/${base}_methylation.bed.gz
         tabix -p bed ${out}/${base}_methylation.bed.gz
 	rm ${out}/${base}_methylation.tmp
