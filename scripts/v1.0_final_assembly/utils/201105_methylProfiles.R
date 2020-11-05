f5a1cc5012f55ad55f651eaf2c8a520543d814ae
gmoneyomics <agershm2@jhmi.edu>
2020-11-05 15:51:20 -0500

    change methyl profile for ai

--- scripts/v1.0_final_assembly/utils/201105_methylProfiles.R
+++ scripts/v1.0_final_assembly/utils/201105_methylProfiles.R
@@ -59,8 +59,8 @@ repeatColors =c("(CATTC)n" = "#E87C71",
                 "RNA"="#54C0A5",
                 "rRNA"="#52BEBB",
                 "SAR"="#51BDCE",
-                "Satellite/acro"="#4EB8DF",
-                "Satellite/telo"="#53B0E3",
+                "Satellite"="#4EB8DF",
+             #   "Satellite"="#53B0E3",
                 "SATR1"="#5AA5DA",
                 "scRNA"="#6B9AD2",
                 "Simple_repeat"="#8992C8",
@@ -68,7 +68,7 @@ repeatColors =c("(CATTC)n" = "#E87C71",
                 "snRNA"="#A885BC",
                 "srpRNA"="#B67EB6",
                 "SST1"="#C378B2",
-                "Satellite/subtelo"="#D173AF",
+                "Satellite"="#D173AF",
                 "tRNA"="#ED72A5",
                 "Unknown"="#EF768C", 
                 "(GAATG)n"="#ff4000",
@@ -77,7 +77,72 @@ repeatColors =c("(CATTC)n" = "#E87C71",
 
 defaultColor = "#000080"
 
-i=1
+censatColors =c("(CATTC)n" = "#E87C71",
+                "(GAATC)n"="#E28455",
+                "HOR"="#D78C32",
+                "BSAT"="#E370AB",
+                "CER" = "#CE9334",
+                "HSAT2"="#C19935",
+                "HSAT1"="#A2A638",
+                "HSAT3"="#8CAC3E",
+                "Low_complexity"="#75B042",
+                "LSAU"="#54B346",
+                "LTR"="#51B756",
+                "MST"="#53BB73",
+                "GSAT"="#55BE8D",
+                "RNA"="#54C0A5",
+                "rRNA"="#52BEBB",
+                "SAR"="#51BDCE",
+                "ACRO"="#4EB8DF",
+                "HSAT4"="#53B0E3",
+                "SATR"="#5AA5DA",
+                "CT"="#6B9AD2",
+                "Simple_repeat"="#8992C8",
+                "SINE"="#9A8AC1",
+                "MON"="#A885BC",
+                "SST"="#C378B2",
+                "HSAT5"="#ED72A5",
+                "HSAT6"="#EF768C", 
+                "gap-rDNA"="#ff4000",
+                "TE" = "#ffbf00", 
+                "TAR"= "#0080ff" )
+
+
+censat = read_tsv(paste0(dat, "/annotations/t2t-chm13.v1.0.cenSat_annotation.bed"), col_names = F) %>%
+  mutate(name = ifelse(grepl("HSat1", X4), "HSAT1", X4)) %>%
+  mutate(name = ifelse(grepl("HSat2", X4), "HSAT2", name)) %>%
+  mutate(name = ifelse(grepl("HSat3", X4), "HSAT3", name)) %>%
+  mutate(name = ifelse(grepl("HSat4", X4), "HSAT4", name)) %>%
+  mutate(name = ifelse(grepl("HSat5", X4), "HSAT5", name)) %>%
+  mutate(name = ifelse(grepl("HSat6", X4), "HSAT6", name)) %>%
+  mutate(name = ifelse(grepl("ct", X4), "CT", name)) %>%
+  mutate(name = ifelse(grepl("bsat", X4), "BSAT", name)) %>%
+  mutate(name = ifelse(grepl("hor", X4), "HOR", name)) %>%
+  mutate(name = ifelse(grepl("mon", X4), "MON", name)) %>%
+  mutate(name = ifelse(grepl("Alu", X4), "TE", name)) %>%
+  mutate(name = ifelse(grepl("SATR", X4), "SATR", name)) %>%
+  mutate(name = ifelse(grepl("ACRO", X4), "ACRO", name)) %>%
+  mutate(name = ifelse(grepl("GSATII", X4), "GSATII", name)) %>%
+  mutate(name = ifelse(grepl("TAR", X4), "TAR", name)) %>%
+  mutate(name = ifelse(grepl("TE", X4), "TE", name)) %>%
+  mutate(name = ifelse(grepl("MER", X4), "TE", name)) %>%
+  mutate(name = ifelse(grepl("MST", X4), "MST", name)) %>%
+  mutate(name = ifelse(grepl("CER", X4), "CER", name)) %>%
+  mutate(name = ifelse(grepl("L1", X4), "TE", name)) %>%
+  mutate(name = ifelse(grepl("SST", X4), "SST", name)) %>%
+  mutate(name = ifelse(grepl("LSAU", X4), "LSAU", name)) %>%
+  mutate(name = ifelse(grepl("GSAT", X4), "GSAT", name)) %>%
+  mutate(name = ifelse(grepl("MSAT", X4), "MSAT", name)) %>%
+  mutate(name = ifelse(grepl("novel", X4), "novel", name)) %>%
+  mutate(name = ifelse(grepl("HERV", X4), "TE", name)) %>%
+  mutate(name = ifelse(grepl("LT", X4), "TE", name)) %>%
+  dplyr::select(c(X1, X2, X3, name)) %>%
+  dplyr::rename("chrom" =1, "start" = 2 ,"end" =3) %>%
+  group_by(name) %>%
+  filter(n() >= 3) %>%
+  ungroup()
+
+
 for (i in 1:length(cen$chr)){
   chr=cen$chr[i]
   start=cen$start[i]
@@ -87,8 +152,8 @@ bis <- as.data.frame(freq.matched) %>%
   filter(seqnames == chr) %>%
   arrange(start) %>%
  # summarise(meth_freq = mean(mcall), cov = n()) %>%
-  mutate(smooth = rollmean(meth, 200, fill = NA)) %>%
-  mutate(cov_smooth = rollmean(cov, 200, fill = NA))
+  mutate(smooth = rollmean(meth, 500, fill = NA)) %>%
+  mutate(cov_smooth = rollmean(cov, 10000, fill = NA))
 
 rm_sub <- rm %>%
   filter(X1 == chr) %>%
@@ -103,15 +168,25 @@ cov <- ggplot(bis, aes(x = (start/1e6), y= cov_smooth))+geom_line(size =1) + lab
 rep_leg <- ggplot(data=rm_sub, mapping=aes(xmin=(X2/1e6),xmax=(X3/1e6),ymin=0,ymax=.1, fill = X7))+
   geom_rect()+theme(legend.position="top") +labs(y="Axis")+xlim(start/1e6,end/1e6) +labs(y="Axis")+ scale_fill_manual(values = repeatColors, drop = FALSE)+theme(legend.text=element_text(size=rel(1.5)))+theme(legend.title=element_blank()) +theme(axis.title.y=element_blank(),axis.text.y=element_blank(), axis.ticks.y=element_blank())
 
-top_row <- plot_grid(rep_leg,meth,cov, ncol = 1, align="v", rel_heights = c(1/4, 1/2, 1/2))
+
+censat_reg <- censat %>%
+  filter(chrom == chr) 
+
+rep_leg2 <- ggplot(data=censat_reg, mapping=aes(xmin=(start/1e6),xmax=(end/1e6),ymin=0,ymax=.1, fill = name))+
+  geom_rect()+theme(legend.position="top") +labs(y="Axis")+xlim(start/1e6,end/1e6) +labs(y="Axis")+ scale_fill_manual(values = censatColors, drop = FALSE)+theme(legend.text=element_text(size=rel(1.5)))+theme(legend.title=element_blank()) +theme(axis.title.y=element_blank(),axis.text.y=element_blank(), axis.ticks.y=element_blank())# +  theme_void()
+
+
+top_row <- plot_grid(rep_leg,rep_leg2,meth,cov, ncol = 1, align="v", rel_heights = c(1/4,1/4, 1/2, 1/2))
 top_row
 
+
 ggsave(
-  paste0(figs, "/", chr, "_HOR_methyl_pattern.pdf"),
+  paste0(figs, "/", chr, "_HOR_methyl_pattern.eps"),
   plot = top_row,
   scale = 1,
-  width = 12,
-  height = 15,
+  width = 10,
+  height = 12,
+  dpi = 50
 )
 
 }
